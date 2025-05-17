152176ba20ed02ad768f6bc29e6bae7834088e64
drcaina <drcaina@gmail.com>
2025-05-17 13:37:29 -0500

    Numeros primo

--- test.py
+++ test.py
@@ -3,3 +3,13 @@ a = 5
 b = 3
 suma = a + b
 print("La suma es:", suma)
+
+# Buscar números primos en el rango del 1 al 50
+print("Números primos del 1 al 50:")
+for num in range(1, 51):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
